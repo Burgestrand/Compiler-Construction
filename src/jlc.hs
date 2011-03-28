8ed150d9b260292b262ad8e95b5b300fc45a56fd
Kim Burgestrand <kim@burgestrand.se>
2011-03-28 19:23:28 +0200

    Lägg till en testfil som lexar,parsear och printar källkod

---
+++ src/jlc.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import ErrM
+import Parjavalette
+
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+
+-- | Compile the file at the given path containing jasmine code.
+jasmin :: FilePath -> IO ()
+jasmin = undefined
+
+-- aliases
+parselex = pProgram . myLexer
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [file] -> do
+      program <- parselex `fmap` readFile file
+      case program of
+        Ok  x -> print x
+        Bad s -> error s
+    _      -> error "Usage: jlc path/to/javalette/source.jl"
