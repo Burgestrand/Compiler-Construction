d4869948bb9c3cfbe6f7a6c7a2c8af62ba0d34fb
Kim Burgestrand <kim@burgestrand.se>
2011-04-01 14:47:58 +0200

    Se till så att Env.hs kompilerar

--- src/Env.hs
+++ src/Env.hs
@@ -1,8 +1,15 @@
-module Env where
+module Env (
+  emptyEnv, add, lookup, addScope
+) where
+
+import AST
+import ErrM
+import Printer
 
 import qualified Data.Map as Map
+import Prelude hiding (lookup)
 
-type Scope = Map.Map Identifier Type
+type Scope = Map.Map Ident Type
 type Env   = [Scope]
 
 -- creates a empty enviroment
@@ -10,12 +17,12 @@ emptyEnv :: Env
 emptyEnv = [Map.empty]
 
 -- adds a identifier to the current scope
-add :: Env -> Identifier -> Type -> Err Env
-add (scope:rest) x t | Map.member x scope = fail $ "Identifier " ++ printTree x ++ " already declared."
+add :: Env -> Ident -> Type -> Err Env
+add (scope:rest) x t | Map.member x scope = fail $ "Ident " ++ printTree x ++ " already declared."
                      | otherwise          = return ((Map.insert x t scope):rest)
 
 -- finds a variable in the enviroment
-lookup :: Env -> Identifier -> Err Type
+lookup :: Env -> Ident -> Err Type
 lookup []           x = fail $ "Unknown identifier " ++ printTree x ++ "."
 lookup (scope:rest) x | Map.member x scope = return $ (Map.!) scope x
                       | otherwise          = lookup rest x
