dd3422ccf93eee8a8ab45e289bdd2dc2d2b420fc
Kim Burgestrand <kim@burgestrand.se>
2011-04-15 19:29:01 +0200

    Make the compiler compile method signatures

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -6,21 +6,62 @@ import Control.Monad.Identity
 import Control.Monad.State
 import Control.Monad.Writer
 
-type Compiling = WriterT Code (StateT Compilation Identity)
+import Data.List
 
-type Code = [String]
+---
+
+type Jasmin = StateT Compilation (Writer Lines)
+
+type Lines = [Code]
+type Code  = String
 data Compilation = Compilation {
   stacksize   :: Integer, -- | Your average counter
   maxstack    :: Integer  -- | Highest value the counter ever reaches
 }
 
-compile :: String -> Program -> String
-compile name program = (++) header $ runIdentity $ do
-    let (Program xs) = program
-    return (show xs)
+---
+
+class Compileable x where
+  assemble :: x -> Jasmin Code
+
+emit :: Code -> Jasmin Code
+emit x = tell [x] >> return x
+
+-- | Emit a directive with the specified name and parameters.
+directive :: String -> String -> Jasmin Code
+directive name args = emit ("." ++ name ++ " " ++ args)
+
+-- | Emit a method declaration and then assemble the contents.
+method :: (Compileable x) => String -> [Arg] -> x -> Jasmin Code
+method name args body = undefined
+
+---
+
+instance Compileable Definition where
+  assemble (Definition return (Ident name) args (Block code)) = do
+    args <- intercalate ";" `fmap` mapM assemble args
+    directive "method" ("public static " ++ name ++ "(" ++ args ++ ")")
+    directive "end" "method"
+
+instance Compileable Arg where
+  assemble (Arg t x) = return $ case t of
+    TInt    -> "I"
+    TDouble -> "D"
+    TBool   -> "B"
+
+---
+
+compiler :: (Compileable x) => x -> Code
+compiler x = intercalate "\n" $ execWriter $ runStateT (assemble x) (Compilation 0 0) 
+
+---
+
+compile :: String -> Program -> Code
+compile name (Program fs) = header ++ functions
   where header = unlines [".class public " ++ name,
                           ".super java/lang/Object",
                           ".method public static main([Ljava/lang/String;)V",
                           "  invokestatic " ++ name ++ "/main()I",
                           "  invokestatic java/lang/System/exit(I)V",
                           ".end method"] ++ "\n"
+        functions = intercalate "\n\n" (map compiler fs)
