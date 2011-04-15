bfae09067660f39009739695913327441cd556ac
Kim Burgestrand <kim@burgestrand.se>
2011-04-15 17:38:11 +0200

    Use the new compilation interface in jlc.hs

--- src/jlc.hs
+++ src/jlc.hs
@@ -5,8 +5,14 @@ import Parser
 import TypeChecker
 import Compiler
 
+import Data.Char (toUpper)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.FilePath (dropExtensions, takeBaseName)
+
+-- utility
+titleize :: String -> String
+titleize (x:xs) = (toUpper x):xs
 
 -- aliases
 parselex = pProgram . myLexer
@@ -16,11 +22,12 @@ main = do
   args <- getArgs
   case args of
     [file] -> do
+      let name = titleize $ dropExtensions (takeBaseName file)
       source  <- readFile file
       program <- return $ do
         program <- parselex source
         program <- typecheck program
-        return (compile program)
+        return $ compile name program
       
       case program of
         Ok  source  -> putStr source
