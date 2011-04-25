6abdc18b1d8c10eedb07c088db3449bf1ddba778
Kim Burgestrand <kim@burgestrand.se>
2011-04-25 19:46:36 +0200

    Refactor Runtime to always use println

--- src/Runtime.java
+++ src/Runtime.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 class Runtime
 {
-  public static void printInt(int n) { System.out.print(n); }
-  public static void printDouble(double x) { System.out.print(x); }
-  public static void printString(String s) { System.out.print(s); }
+  public static void printInt(int n) { System.out.println(n); }
+  public static void printDouble(double x) { System.out.println(x); }
+  public static void printString(String s) { System.out.println(s); }
   
   public static int readInt() { return getScanner().nextInt(); }
   public static double readDouble() { return getScanner().nextDouble(); }
