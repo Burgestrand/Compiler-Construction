82f0ca28764b44dd6c2eb124179180da2db593fe
Kim Burgestrand <kim@burgestrand.se>
2011-04-25 19:26:53 +0200

    Make JLC write and compile (using java) the final assembly code

--- src/jlc.hs
+++ src/jlc.hs
@@ -11,6 +11,8 @@ import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.FilePath (dropExtensions, takeBaseName)
 import System.IO
+import System.Process (rawSystem)
+import System.Exit
 
 -- utility
 titleize :: String -> String
@@ -18,20 +20,25 @@ titleize (x:xs) = (toUpper x):xs
 
 -- | For some reason, the supplied Grade requires output on stderr!?
 info :: String -> IO ()
-info msg = hPutStr stderr msg
+info msg = hPutStr stderr (msg ++ "\n")
+
+-- | Fail with an error message to STDERR.
+fatal msg = info msg >> exitFailure
 
 -- aliases
 parselex = pProgram . myLexer
 
 -- | Compile a file given its’ name.
-jasmin source = undefined
+jasmin :: String -> IO ExitCode
+jasmin source = rawSystem "java" ["-jar", "lib/jasmin.jar", source]
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
       [file] -> do
-        let name = titleize $ dropExtensions (takeBaseName file)
+        let name   = titleize $ dropExtensions (takeBaseName file)
+        let target = name ++ ".j"
         source  <- readFile file
         program <- return $ do
           program <- parselex source
@@ -40,12 +47,15 @@ main = do
         
         case program of
           (Bad message) -> do
-            info $ "ERROR: " ++ message
-            exitFailure
+            fatal $ "ERROR: " ++ message
           
           (Ok source) -> do
             info "OK"
-            writeFile (name ++ ".j") source
+            writeFile target source   -- write assembled code
+            compiled <- jasmin target -- compile assembled code
+            case compiled of
+              (ExitFailure n) -> fatal $ "Compilation failed with exit code " ++ (show n)
+              _               -> return ()
       
       _      -> error "Usage: jlc path/to/javalette/source.jl"
   where
