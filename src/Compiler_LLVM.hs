088e66f68181f778ce45c1a60f0112bb6f7d3e30
Per Reimers <per.reimers@gmail.com>
2011-05-13 15:19:07 +0200

    Adda till Compiler LLVM

---
+++ src/Compiler_LLVM.hs
@@ -0,0 +1,36 @@
+module Compiler (compile) where
+
+import AST
+
+import Control.Monad.Identity
+import Control.Monad.State
+import Control.Monad.Writer
+
+import Data.List
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe
+
+--
+
+data BasicBlock = Returning   Lines 
+                | Jumping     Lines Label 
+                | Conditional Lines Label Label
+
+type LLVM = StateT Compilation (Writer (Map Label BasicBlock))
+
+type Lines = [Code]
+type Code  = String
+data Compilation = Compilation {
+  -- | Class name
+  name :: String,
+  
+  -- | Lists all locals
+  locals :: [Ident],
+  
+  -- | Label counter
+  label :: Integer
+  
+  -- | Temp variable counter
+  temp :: Integer
+}
