f4045e91d5b7e3343246aa5ffa0b7cb84aea23dc
Kim Burgestrand <kim@burgestrand.se>
2011-05-17 14:59:23 +0200

    Bogus now compiles core002!

--- src/Compiler/Bogus.hs
+++ src/Compiler/Bogus.hs
@@ -10,7 +10,7 @@ compile :: String -> Program -> String
 compile name program = unlines
   ["declare void @printString(i8*)",
    "",
-   "@g_1 = internal constant [13 x i8] c\"foo\\0A\\00\"",
+   "@g_1 = internal constant [4 x i8] c\"foo\\00\"",
    "",
    "define i32 @main()",
    "{",
@@ -22,7 +22,7 @@ compile name program = unlines
    "define void @foo()",
    "{",
    "entry:",
-   "  %l_1 = getelementptr [ 13 x i8 ]* @g_1, i32 0, i32 0",
+   "  %l_1 = getelementptr [4 x i8]* @g_1, i32 0, i32 0",
    "  call void @printString(i8* %l_1)",
    "  ret void",
    "}"]
